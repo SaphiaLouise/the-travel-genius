81de6100694bb27c22147c34faea8cdbc38edeea
Saphia Louise <S_Capasso8@yahoo.com>
2019-10-17 15:47:16 -0400

    adding java

---
+++ about/js/main.js
@@ -0,0 +1,10 @@
+
+$('div').on('click', function (){
+	$(this).animate({
+		"opacity": 0.2,
+		"width": 800,
+		"height": 200,
+	}, 4000);
+});
+
+
---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+$('div').on('click', function (){
+	$(this).animate({
+		"opacity": 1,
+		"width": 900,
+		"height": 550,
+	}, 2000);
+});
+
+
+// $('#north').on('click', function(){
+// 	if (animated === false) {
+// 	animated = true;
+// 	$(this).animate ({
+// 		width: 450,
+// 		height: '450px',
+// 		opacity: 0.5,
+// 	});
+// }
+// 	else {
+// 		animated = false;
+// 		$(this).animate ({
+// 		width: 200,
+// 		height: '200px',
+// 		opacity: 1,
+// 	});
+// 	}
+// });
+
+// $('#north').on('click', function (){
+// 	$('#north').animate({
+// 		"opacity": 0.5,
+// 		"width": 300,
+// 		"height": 500,
+// 	}, 3000);
+// })
